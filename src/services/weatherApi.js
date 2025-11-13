3393fd74e3fe807af9189a6aafaa120401b69798
anup <anup.shakya60@gmail.com>
2025-11-13 23:44:21 +0000

    key added

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = "YOUR_API_KEY"
+const API_KEY = "c5db50e53616eb725f9c9cd3824e3753"
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 export async function fetchWeatherByCity( city, signal ) {
